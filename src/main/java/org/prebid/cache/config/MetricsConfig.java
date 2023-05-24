66d74219b035b31514ca862571ef207a20f99673
Alex Maltsev <and1sscsgo@gmail.com>
2023-05-24 13:47:50 +0300

    Added prefix for graphite export

--- src/main/java/org/prebid/cache/config/MetricsConfig.java
+++ src/main/java/org/prebid/cache/config/MetricsConfig.java
@@ -2,7 +2,10 @@ package org.prebid.cache.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.NamingConvention;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +16,17 @@ public class MetricsConfig {
     MeterRegistryCustomizer<MeterRegistry> identityNamingConventionMeterCustomizer() {
         // To preserve metric names as they defined in code
         return registry -> registry.config().namingConvention(NamingConvention.identity);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "management.metrics.export.graphite.enabled", havingValue = "true")
+    MeterRegistryCustomizer<MeterRegistry> graphitePrefixMeterCustomizer(
+        @Value("${management.metrics.export.graphite.prefix:}") String prefix) {
+
+        return registry -> {
+            if (StringUtils.isNotEmpty(prefix)) {
+                registry.config().commonTags("prefix", prefix);
+            }
+        };
     }
 }
