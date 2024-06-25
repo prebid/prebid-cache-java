0ee311a7080e3368fa4ce3143e482cb112f34890
Alex Maltsev <and1sscsgo@gmail.com>
2024-06-25 15:38:54 +0300

    Reverted unnecessary app config changes.

--- src/main/java/org/prebid/cache/routers/ApiConfig.java
+++ src/main/java/org/prebid/cache/routers/ApiConfig.java
@@ -6,11 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Configuration
+@Validated
 @ConfigurationProperties(prefix = "api")
 public class ApiConfig {
 
