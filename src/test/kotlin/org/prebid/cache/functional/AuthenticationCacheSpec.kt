fc1ef21f80e5d61e4aea29136c85ec28e41baca1
osulzhenko <125548596+osulzhenko@users.noreply.github.com>
2025-02-19 17:32:26 +0200

    Add functional tests for external-uuid-security (#157)

--- src/test/kotlin/org/prebid/cache/functional/AuthenticationCacheSpec.kt
+++ src/test/kotlin/org/prebid/cache/functional/AuthenticationCacheSpec.kt
@@ -5,6 +5,7 @@ import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.beEmpty
+import io.kotest.matchers.string.shouldContain
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.contentType
 import org.prebid.cache.functional.BaseSpec.Companion.prebidCacheConfig
@@ -13,6 +14,7 @@ import org.prebid.cache.functional.model.request.RequestObject
 import org.prebid.cache.functional.model.request.TransferValue
 import org.prebid.cache.functional.service.ApiException
 import org.prebid.cache.functional.util.getRandomString
+import org.prebid.cache.functional.util.getRandomUuid
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 
 class AuthenticationCacheSpec : ShouldSpec({
@@ -102,7 +104,7 @@ class AuthenticationCacheSpec : ShouldSpec({
         // when: POST cache endpoint is called
         val exception = shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject, apiKey = null) }
 
-        // then: Bad Request exception is thrown
+        // then: Unauthorized exception is thrown
         assertSoftly {
             exception.statusCode shouldBe UNAUTHORIZED.value()
             exception.responseBody should beEmpty()
@@ -126,7 +128,7 @@ class AuthenticationCacheSpec : ShouldSpec({
         val exception =
             shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject, apiKey = "") }
 
-        // then: Bad Request exception is thrown
+        // then: Unauthorized exception is thrown
         assertSoftly {
             exception.statusCode shouldBe UNAUTHORIZED.value()
             exception.responseBody should beEmpty()
@@ -150,7 +152,7 @@ class AuthenticationCacheSpec : ShouldSpec({
         val exception =
             shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject, apiKey = getRandomString()) }
 
-        // then: Bad Request exception is thrown
+        // then: Unauthorized exception is thrown
         assertSoftly {
             exception.statusCode shouldBe UNAUTHORIZED.value()
             exception.responseBody should beEmpty()
@@ -175,10 +177,189 @@ class AuthenticationCacheSpec : ShouldSpec({
         val exception =
             shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject, apiKey = prebidApiKey.uppercase()) }
 
-        // then: Bad Request exception is thrown
+        // then: Unauthorized exception is thrown
         assertSoftly {
             exception.statusCode shouldBe UNAUTHORIZED.value()
             exception.responseBody should beEmpty()
+        }
+    }
+
+    should("should save JSON transfer value with proper api-key in header when cache-write-secured and external-uuid-secured are enabled") {
+        // given: Prebid Cache with external-uuid-secured=true property
+        val prebidApiKey = getRandomString()
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = true,
+                externalUuidSecured = true,
+                apiKey = prebidApiKey
+            )
+        )
+
+        // and: Request object with JSON transfer value
+        val requestObject = RequestObject.getDefaultJsonRequestObject()
+        val requestTransferValue = objectMapper.readValue(requestObject.puts[0].value, TransferValue::class.java)
+
+        // when: POST cache endpoint is called
+        val postResponse = prebidCacheApi.postCache(requestObject, apiKey = prebidApiKey)
+
+        // when: GET cache endpoint is called
+        val getCacheResponse = BaseSpec.getPrebidCacheApi().getCache(postResponse.responses[0].uuid)
+
+        // then: response content type is the same as request object type
+        getCacheResponse.contentType()?.contentType shouldBe "application"
+        getCacheResponse.contentType()?.contentSubtype shouldBe requestObject.puts[0].type.getValue()
+
+        // and: transfer value is returned
+        val responseTransferValue = objectMapper.readValue(getCacheResponse.bodyAsText(), TransferValue::class.java)
+
+        assertSoftly {
+            responseTransferValue.adm shouldBe requestTransferValue.adm
+            responseTransferValue.width shouldBe requestTransferValue.width
+            responseTransferValue.height shouldBe requestTransferValue.height
+        }
+    }
+
+    should("should save JSON transfer value with proper api-key in header when cache-write-secured disabled and external-uuid-secured enabled") {
+        // given: Prebid Cache with external-uuid-secured=true property
+        val prebidApiKey = getRandomString()
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = false,
+                externalUuidSecured = true,
+                apiKey = prebidApiKey
+            )
+        )
+
+        // and: Request object with JSON transfer value
+        val requestObject = RequestObject.getDefaultJsonRequestObject()
+        val requestTransferValue = objectMapper.readValue(requestObject.puts[0].value, TransferValue::class.java)
+
+        // when: POST cache endpoint is called
+        val postResponse = prebidCacheApi.postCache(requestObject, apiKey = prebidApiKey)
+
+        // when: GET cache endpoint is called
+        val getCacheResponse = BaseSpec.getPrebidCacheApi().getCache(postResponse.responses[0].uuid)
+
+        // then: response content type is the same as request object type
+        getCacheResponse.contentType()?.contentType shouldBe "application"
+        getCacheResponse.contentType()?.contentSubtype shouldBe requestObject.puts[0].type.getValue()
+
+        // and: transfer value is returned
+        val responseTransferValue = objectMapper.readValue(getCacheResponse.bodyAsText(), TransferValue::class.java)
+
+        assertSoftly {
+            responseTransferValue.adm shouldBe requestTransferValue.adm
+            responseTransferValue.width shouldBe requestTransferValue.width
+            responseTransferValue.height shouldBe requestTransferValue.height
+        }
+    }
+
+    should("should save JSON transfer value with proper api-key in header when cache-write-secured and external-uuid-secured are disabled") {
+        // given: Prebid Cache with external-uuid-secured=false property
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = false,
+                externalUuidSecured = false,
+                apiKey = getRandomString()
+            )
+        )
+
+        // and: Request object with JSON transfer value
+        val requestObject = RequestObject.getDefaultJsonRequestObject()
+        val requestTransferValue = objectMapper.readValue(requestObject.puts[0].value, TransferValue::class.java)
+
+        // when: POST cache endpoint is called
+        val postResponse = prebidCacheApi.postCache(requestObject)
+
+        // when: GET cache endpoint is called
+        val getCacheResponse = BaseSpec.getPrebidCacheApi().getCache(postResponse.responses[0].uuid)
+
+        // then: response content type is the same as request object type
+        getCacheResponse.contentType()?.contentType shouldBe "application"
+        getCacheResponse.contentType()?.contentSubtype shouldBe requestObject.puts[0].type.getValue()
+
+        // and: transfer value is returned
+        val responseTransferValue = objectMapper.readValue(getCacheResponse.bodyAsText(), TransferValue::class.java)
+
+        assertSoftly {
+            responseTransferValue.adm shouldBe requestTransferValue.adm
+            responseTransferValue.width shouldBe requestTransferValue.width
+            responseTransferValue.height shouldBe requestTransferValue.height
+        }
+    }
+
+    should("throw an exception when api key is missing for request cache_write_secured=true and external_uuid_Secured=true") {
+        // given: Prebid Cache with external-uuid-secured=true property
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = true,
+                externalUuidSecured = true,
+                apiKey = getRandomString()
+            )
+        )
+
+        // and: Request object with set payload transfer key
+        val requestObject = RequestObject.getDefaultJsonRequestObject().apply { puts[0].key = getRandomUuid() }
+
+        // when: POST cache endpoint is called
+        val exception = shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject) }
+
+        // then: Unauthorized exception is thrown
+        assertSoftly {
+            exception.statusCode shouldBe UNAUTHORIZED.value()
+            exception.responseBody should beEmpty()
+        }
+    }
+
+    should("throw an exception when api key is missing for request and external_uuid_Secured=true") {
+        // given: Prebid Cache with external-uuid-secured=true property
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = false,
+                externalUuidSecured = true,
+                apiKey = getRandomString()
+            )
+        )
+
+        // and: Request object with set payload transfer key
+        val requestObject = RequestObject.getDefaultJsonRequestObject().apply { puts[0].key = getRandomUuid() }
+
+        // when: POST cache endpoint is called
+        val exception = shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject) }
+
+        // then: Unauthorized exception is thrown
+        assertSoftly {
+            exception.statusCode shouldBe UNAUTHORIZED.value()
+            exception.responseBody shouldContain "\"message\":\"Prebid cache host forbids specifying UUID in request by unauthorized users.\""
+        }
+    }
+
+    should("throw an exception when api key is mismatched for request and external-uuid-secured=true") {
+        // given: Prebid Cache with external-uuid-secured=true property
+        val prebidCacheApi = BaseSpec.getPrebidCacheApi(
+            prebidCacheConfig.getBaseRedisConfig(
+                allowExternalUuid = true,
+                cacheWriteSecured = false,
+                externalUuidSecured = true,
+                apiKey = getRandomString()
+            )
+        )
+
+        // and: Request object with set payload transfer key
+        val requestObject = RequestObject.getDefaultJsonRequestObject().apply { puts[0].key = getRandomUuid() }
+
+        // when: POST cache endpoint is called
+        val exception = shouldThrowExactly<ApiException> { prebidCacheApi.postCache(requestObject, apiKey = getRandomString()) }
+
+        // then: Unauthorized exception is thrown
+        assertSoftly {
+            exception.statusCode shouldBe UNAUTHORIZED.value()
+            exception.responseBody shouldContain "\"message\":\"Prebid cache host forbids specifying UUID in request by unauthorized users.\""
         }
     }
 })
